5a0d2b2f2e211c276c0dc0af2030a3ce5e6e402d
MohammadShahbaaz <shahbaazmohd784@gmail.com>
2025-01-31 12:53:43 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# PC-Builder
+A website which helps people build PCs by helping people get the correct compatible components
+
+# How to run:
+Installing dependencies
+```shell
+npm install
+```
+
+# Running in dev environment:
+```shell
+npm run dev
+```
+
+# Running in standard encironment:
+```shell
+npm run server
+```
+**Note:** The main difference between standard and dev environment here is that the dev environment automatically updates the changes preventing you from restarting the server for every change.
---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.set('view engine', 'ejs');
+app.use("/images",express.static("images"));
+app.use("/styles",express.static("styles"));
+
+let prebuild;
+
+fs.readFile('./build_data/pb.json', function (err, data) {
+    if (err) 
+        throw err;
+    prebuild = JSON.parse(data);
+});
+
+app.get('/', (req, res) => {
+    res.render('./home');
+    res.end();
+});
+
+app.get('/build', (req,res) => {
+    res.render('./buildStart');
+    res.end();
+});
+
+app.get('/doc',(req,res) => {
+    res.render('./documentation/doc');
+    res.end();
+});
+
+app.get('/prebuild',(req,res) => {
+    res.render('./options',{ pb:prebuild });
+    res.end();
+});
+
+const intelRouter = require('./routes/intel');
+const amdRouter = require('./routes/amd');
+
+app.use('/intel', intelRouter);
+app.use('/amd', amdRouter);
+
+app.listen(3000);
