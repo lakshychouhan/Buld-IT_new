db5d3a99a8055c0ef7c4b6b1d96a06e9da6ccfce
lakshychouhan <103167584+lakshychouhan@users.noreply.github.com>
2025-04-26 00:27:07 +0530

    First release of Hello World!

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ app.use('/intel', isAuthenticated, intelRouter);
 app.use('/amd', isAuthenticated, amdRouter);
 
 // Start server
-app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
